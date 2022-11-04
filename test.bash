0a683c5002d6a0bd6511271e43a851013927b1eb
Kenta Tsukino <s21c1082ns@s.chibakoudai.jp>
2022-11-04 19:18:48 +0900

    fix

--- test.bash
+++ test.bash
@@ -11,16 +11,16 @@ res=0
 
 
 ### I/O TEST ###
-out=$(seq 5 | ./plus)
-[ "${out}" = 15 ] || ng ${LINENO}
+out=$(seq 5.0 | ./plus)
+[ "${out}" = 15.0 ] || ng ${LINENO}
 
  ### STRANGE INPUT ###
    out=$(echo あ | ./plus)
-   [ "$?" = 1 ]      || ng ${LINENO}
+   [ "$?" = 1.0 ]      || ng ${LINENO}
    [ "${out}" = "" ] || ng ${LINENO}
 
    out=$(echo | ./plus) #空文字
-   [ "$?" = 1 ]      || ng ${LINENO}
+   [ "$?" = 1.0 ]      || ng ${LINENO}
    [ "${out}" = "" ] || ng ${LINENO}
-   [ "$res" = 0 ] && echo OK
+   [ "$res" = 0.0 ] && echo OK
    exit $res
