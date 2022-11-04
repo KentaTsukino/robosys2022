e564bee7335a140debc45718f200c32e77346b25
Kenta Tsukino <s21c1082ns@s.chibakoudai.jp>
2022-11-04 19:30:19 +0900

    fix

--- test.bash
+++ test.bash
@@ -1,26 +1,26 @@
-#!/bin/bash -xv
-# SPDX-FileCopyrightText: 2022 Takuya Nagamine
+#!/bin/bash
+# SPDX-FileCopyrightText: 2022 Kenta Tsukino
 # SPDX-License-Identifier: BSD-3-Clause
-
+　
 ng () {
-	echo NG at Line $1
-	res=1
-}
+	      echo NG at Line $1
+	            res=1
+	    }
+    res=0
+    ### I/O TEST ###
+    out=$(seq 5 | ./plus)
+    [ "${out}" = 14 ] || ng ${LINENO}
+    　
+    [ "$res" = 0 ] && echo OK
+    exit $res
 
-res=0
+    out=$(echo あ | ./plus)
+    [ "$?" = 1 ]      || ng ${LINENO}
+    [ "${out}" = "" ] || ng ${LINENO}
 
+    out=$(echo | ./plus)
+    [ "$?" = 1 ]      || ng ${LINENO}
+    [ "${out}" = "" ] || ng ${LINENO}
 
-### I/O TEST ###
-out=$(seq 5.0 | ./plus)
-[ "${out}" = 15.0 ] || ng ${LINENO}
-
- ### STRANGE INPUT ###
-   out=$(echo あ | ./plus)
-   [ "$?" = 1.0 ]      || ng ${LINENO}
-   [ "${out}" = "" ] || ng ${LINENO}
-
-   out=$(echo | ./plus) #空文字
-   [ "$?" = 1.0 ]      || ng ${LINENO}
-   [ "${out}" = "" ] || ng ${LINENO}
-   [ "$res" = 0.0 ] && echo OK
-   exit $res
+    [ "$res" = 0 ] && echo OK
+    exit $res
