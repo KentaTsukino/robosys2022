f722ffb564af50fdc401304eb7baa3339e6f33c5
Kenta Tsukino <s21c1082ns@s.chibakoudai.jp>
2022-11-04 19:09:55 +0900

    fix

--- test.bash
+++ test.bash
@@ -1,6 +1,7 @@
 #!/bin/bash -xv
-# SPDX-FileCopyrightText: 2022 Kenta Tsukino
+# SPDX-FileCopyrightText: 2022 Takuya Nagamine
 # SPDX-License-Identifier: BSD-3-Clause
+
 ng () {
 	echo NG at Line $1
 	res=1
@@ -8,18 +9,18 @@ ng () {
 
 res=0
 
+
 ### I/O TEST ###
 out=$(seq 5 | ./plus)
-[ "${out}" = 15.0 ] || ng ${LINENO}
+[ "${out}" = 15 ] || ng ${LINENO}
 
-### STRANGE INPUT ###
-out=$(echo あ | ./plus)
-[ "$?" = 1.0 ]      || ng ${LINENO}
-[ "${out}" = "" ] || ng ${LINENO}
-		    
-out=$(echo | ./plus) #空文字
-[ "$?" = 1.0 ]      || ng ${LINENO}
-[ "${out}" = "" ] || ng ${LINENO}
+ ### STRANGE INPUT ###
+   out=$(echo あ | ./plus)
+   [ "$?" = 1 ]      || ng ${LINENO}
+   [ "${out}" = "" ] || ng ${LINENO}
 
-[ "$res" a= 0.0] && echo OK
-exit $res
+   out=$(echo | ./plus) #空文字
+   [ "$?" = 1 ]      || ng ${LINENO}
+   [ "${out}" = "" ] || ng ${LINENO}
+   [ "$res" = 0 ] && echo OK
+   exit $res
